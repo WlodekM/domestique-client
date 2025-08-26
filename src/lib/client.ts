ff97c151160c3ecefc0f750476abfdf58b3c8616
WlodekM <wlodzio.maly@gmail.com>
2025-08-26 12:33:31 +0300

    better request logging

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,22 @@
+//@ts-nocheck: this shit's fucked, man
+/**
+ * @file a wrapper for domestique.ts that adds a few things and some debug logging
+ */
 import { CChannel, CGuild, Client, CMessage, CUser } from "./domestique.ts/client.ts";
 import { page, showNavbar } from "./stores.ts";
 import { goto } from '$app/navigation';
 
-const client = new Client();
+let client: Client;
+
+export function makeClient() {
+    const wsurl: string = localStorage.getItem('wsurl') ?? "wss://api.chat.eqilia.eu/api/v0/live/ws";
+    const apiurl: string = localStorage.getItem('apiurl') ?? "https://api.chat.eqilia.eu";
+    if (client)
+        client.ws?.close();
+    client = new Client(wsurl, apiurl);
+}
+
+makeClient()
 
 globalThis.CChannel = CChannel
 globalThis.CGuild = CGuild
@@ -13,14 +27,14 @@ export function setupWs() {
     client.ws?.addEventListener('close', _ => {
         console.error('connection closed')
         page.set('login')
-        goto('login');
+        goto('/login');
         showNavbar.set(false)
     })
 
     client.ws?.addEventListener('error', _ => {
         console.error('connection errored')
         page.set('login')
-        goto('login');
+        goto('/login');
         showNavbar.set(false)
     })
 }
@@ -32,18 +46,59 @@ client.emit = function (...args) {
     realEmit.call(client, ...args)
 }
 
-window.client = client;
+globalThis.client = client;
 
 const realFetch = client.cache.requests.fetch;
 
 const requestsList: string[] = []
 
+const isFirefox = typeof InstallTrigger !== 'undefined';
 client.cache.requests.fetch = function fetch(path:string, init: RequestInit): Promise<Response> {
-    console.log(init.method??'GET', path, {trace:new Error()})
+    const traceError = new Error();
+    let trace = '';
+    // sorry, i aint getting a chromium browser just to make logs for that
+    if (isFirefox)
+        trace = traceError.stack!.split('\n')
+            .slice(1,10)
+            .map(l => {
+                const split = l.split('@');
+                let functionName = split.shift();
+                if (!functionName)
+                    return split.join('@');
+                if (functionName.includes('/<'))
+                    functionName = functionName.replace('/<', '<anonymous>')
+                return `${functionName}() @ ${split.join('@')}`;
+            })
+            .join('\n');
+    else trace = traceError;
+    const methodColors = {
+        GET: 'lightgreen',
+        POST: '#39f',
+        OPTIONS: '#aaa'
+    }, STATUS_PREFIX_COLORS = {
+        1: 'lightblue',
+        2: 'lightgreen',
+        3: 'aqua',
+        4: 'red',
+        5: 'darkred'
+    }
+    const returnValue = realFetch(path, init);
+    const shortPath = path.startsWith(client.apiUrl) ? path.replace(client.apiUrl, '') : path;
+    returnValue.then(r => {
+        console.groupCollapsed(`%c${init.method??'GET'}%c ${r.status.toString()}%c${shortPath}`,
+            `color: ${methodColors[init.method??'GET']}`, `color: ${STATUS_PREFIX_COLORS[r.status.toString()[0]]}`)
+        if (typeof trace == 'string') {
+            console.log(`%c${trace}`, 'color: lightblue')
+        } else {
+            console.log(trace)
+        }
+        console.log(path)
+        console.groupEnd()
+    })
     requestsList.push(path)
-    return realFetch(path, init)
+    return returnValue
 }
 
-window.requestsList = requestsList;
+globalThis.requestsList = requestsList;
 
 export default client;
